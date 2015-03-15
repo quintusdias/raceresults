e565f5be08edb4b73504439deaefefbd267f630c
jevans <john.g.evans.ne@gmail.com>
2015-03-15 18:09:42 -0400

    Bumping to 0.1.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='raceresults',
-    version='0.0.0',
+    version='0.1.0',
     author='John Evans',
     author_email='john.g.evans.ne@gmail.com',
     packages=find_packages(),
