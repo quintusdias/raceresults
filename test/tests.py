9abb646a501de6176834cda1e99d6b036f5edc19
John Evans <john.g.evans.ne@gmail.com>
2015-10-17 22:34:07 -0400

    CSRR integration test with mocks

--- test/tests.py
+++ test/tests.py
@@ -1,6 +1,7 @@
 import contextlib  
 import csv
 import os  
+import pkg_resources as pkg
 import sys
 import tempfile
 import unittest
@@ -10,6 +11,60 @@ from lxml import html
 
 from raceresults import command_line as cmd
  
+class TestCSRR(unittest.TestCase):
+
+    def create_membership_file(self, filename, members):
+        with open(filename, 'w', newline='') as csvfile:
+            fieldnames = ['FName', 'LName']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for member in members:
+                fname, lname = member.split()
+            writer.writerow({'FName': fname, 'LName': lname})
+
+    @mock.patch('raceresults.crrr.requests.get')
+    def test_csrr(self, mock_get):
+        """
+        Smoke test for csrr command line script
+        """
+        # First call to requests.get is for the state file for massachusetts
+        # for 2015
+        mock_response1 = mock.Mock()
+        fname = pkg.resource_filename(__name__, 'data/massachusetts_2015.html')
+        with open(fname, 'rt') as fptr:
+            mock_response1.text = fptr.read()
+
+        # 2nd call to requests.get is for the single race in the state file
+        mock_response2 = mock.Mock()
+        fname = pkg.resource_filename(__name__, 'data/Oct17_Landma_set1.shtml')
+        with open(fname, 'rt') as fptr:
+            mock_response2.text = fptr.read()
+
+        # 3rd call to requests.get is for the secondary race in the top-level
+        # race file.
+        mock_response3 = mock.Mock()
+        fname = pkg.resource_filename(__name__, 'data/Oct17_Landma_set2.shtml')
+        with open(fname, 'rt') as fptr:
+            mock_response3.text = fptr.read()
+
+        mock_get.side_effect = [mock_response1, mock_response2, mock_response3]
+
+        with tempfile.TemporaryDirectory() as tdir:
+            with chdir(tdir):
+                memb_file = os.path.join(tdir, 'test.csv')
+                self.create_membership_file(memb_file, ['Dan Chruniak'])
+                args = ['', '-y', '2015', '-m', '10', '-d', '17', '17',
+                        '--ml', memb_file, '-o', 'results.html',
+                        '--verbose', 'warning']
+                with mock.patch('sys.argv', args):
+                    cmd.run_coolrunning()
+
+                with open('results.html') as fptr:
+                    output = fptr.read()
+                
+                self.assertTrue("Dan Chruniak" in output)
+        pass
+
 @contextlib.contextmanager  
 def chdir(dirname=None):  
     curdir = os.getcwd()  
